c045eaba8db001f1be9b52229d9b1511c8ced55d
techdeus <sullivan.marlon@gmail.com>
2018-10-26 11:27:16 -0700

    Fixed syntax errors in seed file

--- database/seed.js
+++ database/seed.js
@@ -1,13 +1,13 @@
 const faker = require('faker');
-const db = require('./index.js');
 const Promise = require('bluebird');
+const db = require('./index.js');
+
 
 const generator = () => {
-  let result = [];
+  const result = [];
 
-  for ( let i = 0; i < 100; i += 1 ) {
-    let random = Math.floor(Math.random() * Math.floor(100));
-    let details = {
+  for (let i = 0; i < 100; i += 1) {
+    const details = {
       plays: faker.random.number(),
       likes: faker.random.number(),
       reposts: faker.random.number(),
@@ -18,29 +18,27 @@ const generator = () => {
       songtime: faker.random.number({ min: 0, max: 300 }),
       commentId: faker.random.number({ min: 0, max: 100 }),
       artistId: faker.random.number({ min: 0, max: 10 }),
-      songId: faker.random.number({ min: 0, max: 10 }) 
+      songId: faker.random.number({ min: 0, max: 10 }),
     };
     result.push(details);
   }
   return result;
 };
 
-var gen = Promise.resolve(generator());
-gen.then( (seeded) => {
-  let item = seeded;
+const gen = Promise.resolve(generator());
+gen.then((seeded) => {
+  const item = seeded;
 
-  for (let i in seeded) {
-        
-    let current = seeded[i];
-
+  for (let i = 0; i < seeded.length; i += 1) {
+    const current = seeded[i];
     const queryStringSongs = 'INSERT INTO songs (plays, likes, reposts) values (?, ?, ?)';
 
-    let postSongs = [
-      current.plays, 
-      current.likes, 
-      current.reposts
+    const postSongs = [
+      current.plays,
+      current.likes,
+      current.reposts,
     ];
-    db.query(queryStringSongs, postSongs, (error, results, fields) =>{
+    db.query(queryStringSongs, postSongs, (error) => {
       if (error) {
         console.log(error.message);
       } else {
@@ -50,15 +48,16 @@ gen.then( (seeded) => {
   }
   return item;
 }).then((seeded) => {
-  for (let i in seeded)  {
-    let current = seeded[i];
+  const item = seeded;
+  for (let i = 0; i < seeded.length; i += 1) {
+    const current = seeded[i];
     const queryStringArtists = 'INSERT INTO artists (name, imageURL) values (?, ?)';
 
-    let postArtists = [
+    const postArtists = [
       current.name,
-      current.imageURL
+      current.imageURL,
     ];
-    db.query(queryStringArtists, postArtists, (error, results, fields) => {
+    db.query(queryStringArtists, postArtists, (error) => {
       if (error) {
         console.log(error.message);
       } else {
@@ -67,41 +66,43 @@ gen.then( (seeded) => {
     });
   }
   return item;
-}).then ((seeded) => {
-  for (let i in seeded)  {
-    let current = seeded[i];
+}).then((seeded) => {
+  const item = seeded;
+  for (let i = 0; i < seeded.length; i += 1) {
+    const current = seeded[i];
     const queryStringComment = 'INSERT INTO comment (text, createdAt, songtime, artist_Id, song_Id) values (?, ?, ?, ?, ?)';
 
-    let postComment = [
+    const postComment = [
       current.text,
       current.createdAt,
       current.songtime,
       current.artistId,
-      current.songId
+      current.songId,
     ];
-    db.query(queryStringComment, postComment, (error, results, fields) => {
+    db.query(queryStringComment, postComment, (error) => {
       if (error) {
         console.log(error.message);
       } else {
         console.log('Data persisted!');
       }
     });
-  }  
+  }
   return item;
-}).then ((seeded) => {
-  for (let i in seeded)  {
-    let current = seeded[i];
+}).then((seeded) => {
+  for (let i = 0; i < seeded.length; i += 1)  {
+    const current = seeded[i];
     const queryStringReply = 'INSERT INTO reply (text, createdAt, comment_Id, artist_Id, song_Id) values (?, ?, ?, ?, ?)';
 
-    let postReply = [
+    const postReply = [
       current.text,
       current.createdAt,
       current.songtime,
       current.commentId,
       current.artistId,
-      current.songId
+      current.songId,
     ];
-    db.query(queryStringReply, postReply, (error, results, fields) => {
+
+    db.query(queryStringReply, postReply, (error) => {
       if (error) {
         console.log(error.message);
       } else {
@@ -109,6 +110,7 @@ gen.then( (seeded) => {
       }
     });
   }
-}).catch(error => {
-  console.log(error);
-});
+})
+  .catch((error) => {
+    console.log(error);
+  });
