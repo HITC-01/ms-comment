e1b84ab05ede2f4549a5c6e819ff6ebaf05fdb1b
techdeus <sullivan.marlon@gmail.com>
2018-11-02 20:12:09 -0700

    Comment Component tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,27 @@
 /**
  * These rules enforce the Hack Reactor Style Guide
  *
+ module.exports = {
+   extends: 'airbnb'
+ };
  * Visit this repo for more information:
  *   https://github.com/reactorcore/eslint-config-hackreactor
  */
 
 module.exports = {
-  extends: 'airbnb'
+  'parser': 'babel-eslint',
+  'extends': 'airbnb',
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true,
+  },
+  'plugins': [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  'rules': {
+    'react/forbid-prop-types': 'warn',
+  }
 };
